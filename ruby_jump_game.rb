20338603799a2be6e862e8b7833b9bdbd10bd7e4
danbender <danbender@rockets-MacBook-Pro-13.local>
2014-12-26 22:35:36 +0100

    add sound

--- ruby_jump_game.rb
+++ ruby_jump_game.rb
@@ -1,28 +1,32 @@
 require "gosu"
 
 class RubyJumpGame < Gosu::Window
-	def initialize width=800, height=600, fullscreen=false
-		super
-		self.caption = "A Ruby Jump Game"
+    def initialize width=800, height=600, fullscreen=false
+        super
+        self.caption = "A Ruby Jump Game"
+        @song  = Gosu::Song.new self, "music.mp3"
+        @song.play
+        @sound = Gosu::Sample.new self, "hop.mp3" 
+        @image = Gosu::Image.from_text self, 
+                       "Hello, this is me.",
+                       Gosu.default_font_name,
+                       100
 
-		@image = Gosu::Image.from_text self, 
-					   "Hello, this is me.",
-					   Gosu.default_font_name,
-					   100
-	end
+    end
 
-	def button_down id
-		close if id == Gosu::KbEscape
-	end
+    def button_down id
+        close if id == Gosu::KbEscape
+        @sample.play if id == Gosu::KbX
+    end
 
-	def update
-		@x = self.width/2 - @image.width/2
-		@y = self.height/2 - @image.height/2
-	end
+    def update
+        @x = self.width/2 - @image.width/2 + Math.sin(Time.now.to_f) * 150
+        @y = self.height/2 - @image.height/2 + Math.tan(Time.now.to_f) * 100
+    end
 
-	def draw
-		@image.draw @x, @y, 0
-	end
+    def draw
+        @image.draw @x, @y, 0
+    end
 end
 
 RubyJumpGame.new.show
