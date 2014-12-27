8416b28b788f9d175933ced4dafa3087f2bbec83
danbender <hello@danbender.net>
2014-12-27 17:28:21 +0100

    move left when arrow left

--- sprite/sprite.rb
+++ sprite/sprite.rb
@@ -1,8 +1,42 @@
 require 'gosu'
 
+class Sprite < Gosu::Window
+	def initialize window
+		@window = window
+		@image = Gosu::Image.new @window, "assets/player.png"
+		# center img
+		@x = @window.width/2 - @image.width/2
+		@y = @window.height/2 - @image.height/2
+	end
+
+	def update
+		@x += -5 if @window.button_down?  Gosu::KbLeft
+	end
+
+	def draw
+		@image.draw @x, @y, 1
+	end
+end
+
+
+
 class SpriteGame < Gosu::Window
-	def initialize width = 800, height = 600, full = false
+	def initialize width = 800, height = 600, fullscreen = false
 		super
+		self.caption = "Sprite Demonstration"
+		@sprite = Sprite.new self
+	end
+
+	def button_down id
+		close if id == Gosu::KbEscape
+	end
+
+	def update
+		@sprite.update
+	end
+
+	def draw
+		@sprite.draw
 	end
 
 end
